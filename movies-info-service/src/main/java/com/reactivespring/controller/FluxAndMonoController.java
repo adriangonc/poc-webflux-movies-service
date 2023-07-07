ec3e328cfd6c4ca2967ada8f71b46278782bb5e4
adriangonc <adriangonc@yahoo.com.br>
2023-07-07 17:14:52 -0300

    Test streams

--- movies-info-service/src/main/java/com/reactivespring/controller/FluxAndMonoController.java
+++ movies-info-service/src/main/java/com/reactivespring/controller/FluxAndMonoController.java
@@ -1,16 +1,26 @@
 package com.reactivespring.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
+import java.awt.*;
+import java.time.Duration;
+
 @RestController
 public class FluxAndMonoController {
 
-    @GetMapping
+    @GetMapping("/number-flux")
     public Flux<Integer> numbersFlux(){
-        return Flux.just(1532, 1533, 1534);
+        return Flux.just(1532, 1533, 1534)
+                .log();
+    }
+
+    @GetMapping(value = "/number-stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Long> numbersStream(){
+        return Flux.interval(Duration.ofMillis(100))
+                .log();
     }
 
 }
-i
