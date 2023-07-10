438ec3d32256e9def4cf7afe0599168f8b50312b
adriangonc <adriangonc@yahoo.com.br>
2023-07-10 16:04:30 -0300

    Implementation of integration tests

--- movies-info-service/src/test/java/integration/com/reactivespring/repository/MovieInfoRepositoryIntegrationTest.java
+++ movies-info-service/src/test/java/integration/com/reactivespring/repository/MovieInfoRepositoryIntegrationTest.java
@@ -12,6 +12,8 @@ import reactor.test.StepVerifier;
 import java.time.LocalDate;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @DataMongoTest
 @ActiveProfiles("test")
 class MovieInfoRepositoryIntegrationTest {
@@ -21,14 +23,14 @@ class MovieInfoRepositoryIntegrationTest {
 
     @BeforeEach
     void setUp(){
-        var movieInfos = List.of(new MovieInfo(null, "Alien", 1979,
+        var movieInfos = List.of(new MovieInfo("MVI_1", "Alien", 1979,
                 List.of("Tom Skerritt" ,"Sigourney Weaver" ,"Veronica Cartwright" ,"Harry Dean Stanton"
                 ,"John Hurt" ,"Ian Holm" ,"Yaphet Kotto"),
                         LocalDate.parse("1979-05-25")),
-                new MovieInfo(null, "Túmulo dos Vaga-Lumes", 1988,
+                new MovieInfo("MVI_2", "Túmulo dos Vaga-Lumes", 1988,
                         List.of("Tsutomu Tatsumi" ,"Ayano Shiraishi" ,"Yoshiko Shinohara" ,"Akemi Yamaguchi"),
                         LocalDate.parse("1988-04-16")),
-                new MovieInfo(null, "Mad Max: Estrada da Fúria", 2015,
+                new MovieInfo("MVI_3", "Mad Max: Estrada da Fúria", 2015,
                         List.of("Tom Hardy" ,"Charlize Theron" ,"Nicholas Hoult"),
                         LocalDate.parse("2015-05-14")));
 
@@ -50,6 +52,21 @@ class MovieInfoRepositoryIntegrationTest {
         //then
         StepVerifier.create(moviesInfoFlux)
                 .expectNextCount(3)
+                .verifyComplete();
+    }
+
+    @Test
+    void findById(){
+        //given
+
+        //when
+        var moviesInfoMono = movieInfoRepository.findById("MVI_2").log();
+
+        //then
+        StepVerifier.create(moviesInfoMono)
+                .assertNext(movieInfo -> {
+                    assertEquals("Túmulo dos Vaga-Lumes", movieInfo.getName());
+                })
                 .verifyComplete();
     }
 
