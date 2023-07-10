b840ed459002cf79e04afc6021c7a0c28e9b42bc
adriangonc <adriangonc@yahoo.com.br>
2023-07-10 15:53:53 -0300

    Implementation of mongodb integration test

--- movies-info-service/src/test/java/integration/com/reactivespring/repository/MovieInfoRepositoryIntegrationTest.java
+++ movies-info-service/src/test/java/integration/com/reactivespring/repository/MovieInfoRepositoryIntegrationTest.java
@@ -1,8 +1,16 @@
 package com.reactivespring.repository;
 
+import com.reactivespring.domain.MovieInfo;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.ActiveProfiles;
+import reactor.test.StepVerifier;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @DataMongoTest
 @ActiveProfiles("test")
@@ -10,5 +18,39 @@ class MovieInfoRepositoryIntegrationTest {
 
     @Autowired
     MovieInfoRepository movieInfoRepository;
+
+    @BeforeEach
+    void setUp(){
+        var movieInfos = List.of(new MovieInfo(null, "Alien", 1979,
+                List.of("Tom Skerritt" ,"Sigourney Weaver" ,"Veronica Cartwright" ,"Harry Dean Stanton"
+                ,"John Hurt" ,"Ian Holm" ,"Yaphet Kotto"),
+                        LocalDate.parse("1979-05-25")),
+                new MovieInfo(null, "Túmulo dos Vaga-Lumes", 1988,
+                        List.of("Tsutomu Tatsumi" ,"Ayano Shiraishi" ,"Yoshiko Shinohara" ,"Akemi Yamaguchi"),
+                        LocalDate.parse("1988-04-16")),
+                new MovieInfo(null, "Mad Max: Estrada da Fúria", 2015,
+                        List.of("Tom Hardy" ,"Charlize Theron" ,"Nicholas Hoult"),
+                        LocalDate.parse("2015-05-14")));
+
+        movieInfoRepository.saveAll(movieInfos).blockLast();
+    }
+
+    @AfterEach
+    void tearDown(){
+        movieInfoRepository.deleteAll().block();
+    }
+
+    @Test
+    void findAll(){
+        //given
+
+        //when
+        var moviesInfoFlux = movieInfoRepository.findAll().log();
+
+        //then
+        StepVerifier.create(moviesInfoFlux)
+                .expectNextCount(3)
+                .verifyComplete();
+    }
 
 }
