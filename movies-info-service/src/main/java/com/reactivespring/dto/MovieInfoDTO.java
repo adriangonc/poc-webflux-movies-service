0381a6ff6b098afa7f9600a50c08063324e78877
adriangonc <adriangonc@yahoo.com.br>
2023-07-14 18:11:13 -0300

    Create movie dto

---
+++ movies-info-service/src/main/java/com/reactivespring/dto/MovieInfoDTO.java
@@ -0,0 +1,19 @@
+package com.reactivespring.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+public class MovieInfoDTO {
+
+    private String name;
+
+    private Integer year;
+
+    private List<String> cast;
+
+    private LocalDate releaseDate;
+
+}
