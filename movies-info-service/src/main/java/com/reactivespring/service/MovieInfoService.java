ec7c3e7697fec02d331e75804ab025ca5615a9e3
adriangonc <adriangonc@yahoo.com.br>
2023-07-24 18:01:09 -0300

    Refactor

--- movies-info-service/src/main/java/com/reactivespring/service/MovieInfoService.java
+++ movies-info-service/src/main/java/com/reactivespring/service/MovieInfoService.java
@@ -1,7 +1,6 @@
 package com.reactivespring.service;
 
 import com.reactivespring.domain.MovieInfo;
-import com.reactivespring.dto.MovieInfoDTO;
 import com.reactivespring.repository.MovieInfoRepository;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
