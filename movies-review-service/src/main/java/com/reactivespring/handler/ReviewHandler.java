d403ba5a940f5304953369b0e37b057c84dcc531
adriangonc <adriangonc@yahoo.com.br>
2023-08-18 09:50:19 -0300

    Creation of post review endpoint

--- movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
+++ movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
@@ -1,4 +1,32 @@
 package com.reactivespring.handler;
 
+import com.reactivespring.domain.Review;
+import com.reactivespring.repository.ReviewRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
 public class ReviewHandler {
+
+    private ReviewRepository reviewRepository;
+
+    public ReviewHandler(ReviewRepository reviewRepository) {
+        this.reviewRepository = reviewRepository;
+    }
+
+    public Mono<ServerResponse> addReview(ServerRequest request) {
+
+        return request.bodyToMono(Review.class)
+                .flatMap(review -> {
+                    return reviewRepository.save(review);
+                })
+                .flatMap(savedReview -> {
+                    return ServerResponse.status(HttpStatus.CREATED)
+                            .bodyValue(savedReview);
+                });
+
+    }
 }
--- movies-review-service/src/main/java/com/reactivespring/repository/ReviewRepository.java
+++ movies-review-service/src/main/java/com/reactivespring/repository/ReviewRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 public interface ReviewRepository extends ReactiveMongoRepository<Review, String> {
 
+
+
 }
