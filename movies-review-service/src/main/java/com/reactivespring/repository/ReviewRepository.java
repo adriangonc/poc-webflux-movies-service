95915c5696798cb3d1361d0a6b93e1038cb535e7
adriangonc <adriangonc@yahoo.com.br>
2023-08-18 09:35:08 -0300

    Creation of repository

--- movies-review-service/src/main/java/com/reactivespring/repository/ReviewRepository.java
+++ movies-review-service/src/main/java/com/reactivespring/repository/ReviewRepository.java
@@ -4,4 +4,5 @@ import com.reactivespring.domain.Review;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 public interface ReviewRepository extends ReactiveMongoRepository<Review, String> {
+
 }
